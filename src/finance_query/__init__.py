943de89cec80268aae75051f6a4a0185b955aaf5
DungLee28 <dung.levc2810@gmail.com>
2026-08-07 04:03:53 +0700

    Initialize finance_query package

---
+++ src/finance_query/__init__.py
@@ -0,0 +1,6 @@
+"""Adaptive hierarchical RAG components for ViFinQA."""
+
+from .schemas import QuestionPlan, RetrievedTable, TableAsset
+
+__all__ = ["QuestionPlan", "RetrievedTable", "TableAsset"]
+__version__ = "0.1.0"
